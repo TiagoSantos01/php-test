6e421a669261640359a0ac9db59bb6261618b157
tiago <tiago@ticogame.com>
2020-06-24 00:34:44 -0300

    array_reveser FileCollection

--- src/FileCollection.php
+++ src/FileCollection.php
@@ -24,8 +24,8 @@ class FileCollection implements CollectionInterface
     public function __construct()
     {
         $this->src_arquivo="CollectionFile.json";
-        $this->json=json_decode(file_get_contents($this->src_arquivo));
         $this->arquivo=fopen($this->src_arquivo,"a+");
+        $this->json=array_reverse(file_get_contents($this->src_arquivo));
     }
 
     /**
@@ -48,7 +48,7 @@ class FileCollection implements CollectionInterface
     {
         $this->json[$index] = $value;
 
-        fwrite($this->src_arquivo, array($this->json));
+        fwrite($this->arquivo, print_r(array($index,$value)));
     }
 
     /**
